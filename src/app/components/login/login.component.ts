322a5efe422d3c97daba780c4738d29048fceea7
Jayakusireddi <jayakusireddy212@gmail.com>
2022-11-03 13:04:34 +0530

    Login reactive form implementation completed

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+login() {
+throw new Error('Method not implemented.');
+}
+
+  public loginUserForm:FormGroup= new FormGroup(
+    {
+      email:new FormControl(null, [Validators.required, Validators.email]),
+      password:new FormControl(null, [Validators.required, Validators.minLength(8),Validators.maxLength(12)])
+
+    }
+  )
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  Login(){
+    
+  }
 }
+
