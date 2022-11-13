4e337ce4b3de88d44bc521ec6f93f9bad71d5d61
Jayakusireddi <jayakusireddy212@gmail.com>
2022-11-13 15:48:55 +0530

    Create student form completed

--- src/app/create-student/create-student.component.ts
+++ src/app/create-student/create-student.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-student',
@@ -21,15 +21,50 @@ export class CreateStudentComponent implements OnInit {
           mandal:new FormControl(),
           district:new FormControl(),
           state:new FormControl(),
-          pincode:new FormControl()
+          pincode:new FormControl(null, [Validators.min(100000),Validators.max(999999)]),
         }
-      )
+      ),
+      company:new FormControl(
+        {
+          name:new FormControl(null,[Validators.minLength(3),Validators.maxLength(10)]),
+          location:new FormControl(),
+          pacakge:new FormControl(null,[Validators.min(100000),Validators.max(999999)]),
+          offerDate:new FormControl()
+        }
+      ),
+      education:new FormArray([]),
+      sourceType:new FormControl(),
+      sourceForm:new FormControl(),
+      referalName:new FormControl()
     }
   )
+  get educationsFormArray(){
+    return this.studentForm.get('educations')as FormArray;
+  }
+  add(){
+    this.educationsFormArray.push(
+      new FormGroup(
+        {
+          qualification:new FormControl(),
+          year:new FormControl(),
+          percentage:new FormControl()
+        }
+      )
+
+    )
+  }
+  delete(i:number){
+    this.educationsFormArray.removeAt(i);
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submit(){
+    console.log(this.studentForm);
+    this.studentForm.markAllAsTouched();
   }
 
 }
