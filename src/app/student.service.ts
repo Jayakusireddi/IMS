9af94336a04a4474a30d24c75874b4f45ae8a32a
Jayakusireddi <jayakusireddy212@gmail.com>
2022-11-13 16:09:37 +0530

    form api integration completed

---
+++ src/app/student.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  constructor(private _httpClient:HttpClient) {}
+
+  createStudent(data:any):Observable<any>{
+    return this._httpClient.post("https://62b9299dff109cd1dc8ca34f.mockapi.io/students",data);
+  }
+   
+}
